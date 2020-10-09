fa25c5b81cc0bf9745523930d8b5991b6d8e8a95
Ashutosh Kumar <ashu.heretocode@gmail.com>
2020-10-09 22:16:50 +0530

    Added wrong input feature

--- app.js
+++ app.js
@@ -4,29 +4,41 @@ var finalLocation='';
 input.addEventListener('keyup',(e)=>{
     if(e.key=='Enter' && text.length>0)
     {
-        finalLocation=text;   
+        finalLocation=text; 
+        finalLocation = finalLocation.toLowerCase();  
         updateInfo(finalLocation);     
     }
     text=e.target.value;
     // console.log(text);
     // console.log(e);
 });
-
+let city= document.querySelector('.city');
+let weather = document.querySelector('.weather');
+let temp = document.querySelector('span');
+let image= document.querySelector('.image');
 const updateInfo= async (location)=>{
     const result = await fetch('./data.json');
     let data = await result.json();
     let final = data.filter(city=>city.city===location);
-    let place = final[0];
-    let city= document.querySelector('.city');
-    let weather = document.querySelector('.weather');
-    let temp = document.querySelector('span');
-    document.querySelector('.image').style.backgroundImage = `url(./images/${place.weather}.jpg)`;
-    city.innerText= place.city.toUpperCase();
-    weather.innerText=place.weather.toUpperCase();
-    temp.innerText= place.temp;
-    // temp.innerText += '&deg C'; 
-    document.querySelector('.card').style.visibility='visible';
-    console.log(final[0]);   
+    if(final.length==0)
+    {        
+        document.querySelector('.card').style.visibility = "visible";       
+        image.style.backgroundImage='linear-gradient(to right, gray, rgb(56, 51, 51))';
+        city.innerText= "INVALID";
+        weather.innerText="NOT FOUND"
+        temp.innerText= "INVALID"
+    }
+    else 
+    {        
+        let place = final[0];        
+        image.style.backgroundImage = `url(./images/${place.weather}.jpg)`;
+        city.innerText= place.city.toUpperCase();
+        weather.innerText=place.weather.toUpperCase();
+        temp.innerText= place.temp;
+        // temp.innerText += '&deg C'; 
+        document.querySelector('.card').style.visibility='visible';    
+        //console.log(final[0]);  
+    } 
 }
 
 
